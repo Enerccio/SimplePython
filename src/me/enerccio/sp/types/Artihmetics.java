ad175dd65fdba184ecf1fda0516cf862a222ee45
EnerccioCLI <enerccio@paldopice.com>
2015-07-18 15:12:32 +0200

    arithmetics

---
+++ src/me/enerccio/sp/types/Artihmetics.java
@@ -0,0 +1,16 @@
+package me.enerccio.sp.types;
+
+public final class Artihmetics {
+
+	public static final String __ADD__ = "__add__";
+	public static final String __SUB__ = "__sub__";
+	public static final String __MUL__ = "__mul__";
+	public static final String __DIV__ = "__div__";
+	public static final String __MOD__ = "__mod__";
+	public static final String __POW__ = "__pow__";
+	public static final String __AND__ = "__and__";
+	public static final String __OR__  = "__or__";
+	public static final String __NOT__ = "__not__";
+	public static final String __XOR__ = "__xor__";
+	
+}
