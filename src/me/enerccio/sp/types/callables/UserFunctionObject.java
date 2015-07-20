fcd877f66da10d9f112f1b9215f50d3b7a541c34
EnerccioCLI <enerccio@paldopice.com>
2015-07-21 01:46:59 +0200

    fixed bug with number of args in function call

--- src/me/enerccio/sp/types/callables/UserFunctionObject.java
+++ src/me/enerccio/sp/types/callables/UserFunctionObject.java
@@ -42,7 +42,11 @@ public class UserFunctionObject extends PythonObject {
 			++rargs;
 		
 		if (argc < rargs)
-			throw Utils.throwException("TypeError", "Incorrect amount of arguments, expected at least " + rargs + ", got " + args);
+			throw Utils.throwException("TypeError", "Incorrect amount of arguments, expected at least " + rargs + ", got " + args.len());
+		
+		if (!isVararg && argc > rargs)
+			throw Utils.throwException("TypeError", "Incorrect amount of arguments, expected at most " + rargs + ", got " + args.len());
+			
 		
 		MapObject a = new MapObject();
 		a.newObject();
